36e00d7d12a3a77cecef59ed43396593a79ea7dc
CsillaCsejtei <csillacsejtei@gmail.com>
2025-08-09 10:55:01 +0100

    Added category map

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,22 +1,38 @@
-// Import required packages
 const sequelize = require("../config/connection");
+const { Post, Category } = require("../models");
 
-// import models
-const { Post } = require("../models");
-
-// add data and seeding for Category model
-
-// import seed data
 const postData = require("./posts.json");
+const categoryData = require("./categories.json");
 
-// Seed database
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
 
-  await Post.bulkCreate(postData);
+  await Category.bulkCreate(categoryData);
 
+  const categories = await Category.findAll();
+
+  const categoryMap = {};
+  categories.forEach((cat) => {
+    categoryMap[cat.category_name] = cat.id;
+  });
+
+  const cleanPosts = postData.map((post) => {
+    const categoryName = post.category || post["category:"];
+    const categoryId = categoryMap[categoryName] || null;
+
+    return {
+      title: post.title,
+      content: post.content,
+      postedBy: post.postedBy,
+      categoryId,
+      createdOn: post.createdOn,
+    };
+  });
+
+  await Post.bulkCreate(cleanPosts);
+
+  console.log("Database seeded!");
   process.exit(0);
 };
 
-// Call seedDatabase function
 seedDatabase();
