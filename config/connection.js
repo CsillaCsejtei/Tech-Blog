156f7d67feb3cc2d8a05526d004843e7b903f7ac
CsillaCsejtei <csillacsejtei@gmail.com>
2025-08-09 20:34:01 +0100

    Updated connection

--- config/connection.js
+++ config/connection.js
@@ -1,23 +1,16 @@
 require("dotenv").config();
-
 const Sequelize = require("sequelize");
 
-if (process.env.DB_PASSWORD === "ChangeMe!") {
-  console.error("Please update the .env file with your database password.");
-  process.exit(1);
-}
-
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
-      process.env.DB_DATABASE,
-      process.env.DB_USERNAME,
-      process.env.DB_PASSWORD,
-      {
-        host: process.env.DB_HOST,
-        dialect: process.env.DB_DIALECT,
-        port: process.env.DB_PORT,
-      }
-    );
+const sequelize = new Sequelize(
+  process.env.DB_DATABASE,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT || "mysql",
+    port: process.env.DB_PORT || 3306,
+    logging: false,
+  }
+);
 
 module.exports = sequelize;
