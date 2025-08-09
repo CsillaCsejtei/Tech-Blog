cb234de61458078ef40755e4bf52b51a73d88f55
CsillaCsejtei <csillacsejtei@gmail.com>
2025-08-09 11:00:26 +0100

    Deleted comments

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// Import required packages
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
@@ -6,28 +5,22 @@ const path = require("path");
 const sequelize = require("./config/connection");
 const routes = require("./routes");
 
-// Initialize Express application
 const app = express();
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
 const PORT = process.env.PORT || 3010;
 
-// has the --rebuild parameter been passed as a command line param?
 const rebuild = process.argv[2] === "--rebuild";
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, "public")));
 
-// Handle GET request at the root route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.htm"));
 });
 
-// Add routes
 app.use(routes);
 
-// Sync database
 sequelize.sync({ force: rebuild }).then(() => {
   app.listen(PORT, () => console.log(`Server is running at http://localhost:${PORT}`));
 }).catch((err) => {
